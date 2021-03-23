aefe7545f9519e8e1260130f87d91ae7752dcfc5
sism1s <marko.lazic880@gmail.com>
2021-03-23 00:21:57 +0000

    Set up firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC9l4QMo4Drlav-JhedtpU2K40ZjZPFkQY",
+  authDomain: "clone-e02e4.firebaseapp.com",
+  projectId: "clone-e02e4",
+  storageBucket: "clone-e02e4.appspot.com",
+  messagingSenderId: "388438669520",
+  appId: "1:388438669520:web:e82e4f95b4d0e775ab86d5",
+};
